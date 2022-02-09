6b7838b1208f6756a2babaac7137266932b4eea3
EGM <ezegonmac01@gmail.com>
2022-02-09 17:00:27 +0100

    Final app

--- src/App.js
+++ src/App.js
@@ -1,25 +1,51 @@
-import logo from './logo.svg';
-import './App.css';
+import { createContext, Component, useContext } from 'react'
+
+const Context1 = createContext('context1')
+const Context2 = createContext('context2')
+
+const Provider = ({ children }) => {
+  return (
+    <Context1.Provider value='valor 1'>
+        <Context2.Provider value='valor 2'>
+            {children}
+        </Context2.Provider>
+    </Context1.Provider>
+  )
+}
+
+// CLASES
+class Componente extends Component {
+    render() {
+        return(
+            <Context1.Consumer>
+                { valor1 => 
+                    <Context2.Consumer>
+                        {
+                            valor2 => <div>{`${valor1} ${valor2}`}</div>
+                        }
+                    </Context2.Consumer>
+                }
+            </Context1.Consumer>
+        )
+    }
+}
+
+// FUNCIONAL
+const Componente2 = () => {
+    const valor1 = useContext(Context1)
+    const valor2 = useContext(Context2)
+    return (
+        <div>{`${valor1} ${valor2}`}</div>
+    )
+}
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  return(
+    <Provider>
+      <Componente />
+      <Componente2 />
+    </Provider>
+  )
 }
 
 export default App;
